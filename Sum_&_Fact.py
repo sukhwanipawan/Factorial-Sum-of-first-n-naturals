2ed610631391778c9bc7ecccaefe32847768516b
sukhwanipawan <58626094+sukhwanipawan@users.noreply.github.com>
2020-02-23 11:12:20 +0530

    Update Sum_&_Fact.py

--- Sum_&_Fact.py
+++ Sum_&_Fact.py
@@ -1,3 +1,5 @@
+from time import *
+
 def sum(A):
     add = 0
     if A == 1:
@@ -5,12 +7,24 @@ def sum(A):
     else:
         add = A + sum(A-1)
         return add
+    print('The sum uptil 1 is', add)
+def factorial(A):
+    fact = 0
+    if A == 1:
+        return 1
+    else:
+        fact = A * factorial(A-1)
+        return fact
 
-
-inp = int(input('Enter a positive number to find sum')) #input from user
+inp = int(input('Enter a positive number to find sum and factorial')) #input from user
 if inp == 0:
-    print("The sum is ZERO")
+    print("The sum is ZERO AND factorial is ONE")
 elif inp == 1:
-    print("The sum is 1")
+    print("The sum AND factorial both are 1")
 elif inp > 1:
-    print(sum(inp))
+    print('The sum uptil 1 is -->', sum(inp))
+    print('The factorial is -->', factorial(inp))
+t = time()
+print('Hah, done in', time()-t)
+
+
